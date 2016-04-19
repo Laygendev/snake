7fe4243982b391534d388c731f816b7329ca8bec
labodudev <lelabodudev@gmail.com>
2016-04-19 19:17:12 +0200

    Move loop to snake module

--- content/srv/socketio/app/socketio-example/server-snake/server-snake.app.js
+++ content/srv/socketio/app/socketio-example/server-snake/server-snake.app.js
@@ -82,7 +82,8 @@ function serverSnake()
 
 	this.sendUpdates = function() {
 		if(self.socket != undefined) {
-			self.moveLoop();
+			if (socket.SERVER.engineArray[2].exec.LoadAppByName('snake') != undefined)
+				socket.SERVER.engineArray[2].exec.LoadAppByName('snake').exec.moveLoop();
 
 			var size = Object.keys(self.socket.SERVER.CLIENTS).length;
 			if(self.socket.SERVER != undefined && self.socket.SERVER.CLIENTS != undefined && size > 0) {
@@ -107,51 +108,6 @@ function serverSnake()
 		}
 
 		setTimeout(self.sendUpdates, 1000 / wf.CONF['SNAKE_CONF'].networkUpdateFactor);
-	}
-
-	this.moveLoop = function() {
-		if(self.socket != undefined) {
-			var size = Object.keys(self.socket.SERVER.CLIENTS).length;
-			if(self.socket.SERVER != undefined && self.socket.SERVER.CLIENTS != undefined && size > 0) {
-				for (var i in self.socket.SERVER.CLIENTS) {
-					if(self.socket.SERVER.CLIENTS[i].player != undefined) {
-						self.tickPlayer(self.socket.SERVER.CLIENTS[i].player);
-					}
-				}
-			}
-		}
-	}
-
-	this.tickPlayer = function(player) {
-		// if(currentPlayer.lastHeartbeat < new Date().getTime() - c.maxHeartbeatInterval) {
-    //   sockets[currentPlayer.id].emit('kick', 'Last heartbeat received over ' + c.maxHeartbeatInterval + ' ago.');
-    //   sockets[currentPlayer.id].emit('RIP');
-    //   sockets[currentPlayer.id].disconnect();
-    // }
-    if (player != undefined) {
-      self.movePlayer(player);
-      // checkCollider(currentPlayer);
-      // eatFood(currentPlayer);
-    }
-	}
-
-	this.movePlayer = function(player) {
-		if (player.d == wf.CONF['SNAKE_CONF'].KEY_LEFT) {
-      player.a -= wf.CONF['SNAKE_CONF'].speedAngle;
-    }
-    if (player.d == wf.CONF['SNAKE_CONF'].KEY_RIGHT) {
-      player.a += wf.CONF['SNAKE_CONF'].speedAngle;
-    }
-
-    player[0] += wf.CONF['SNAKE_CONF'].speed * Math.cos(player.a * Math.PI / 180);
-    player[1] += wf.CONF['SNAKE_CONF'].speed * Math.sin(player.a * Math.PI / 180);
-
-		// if (player.lp.length > 0) {
-	  //   var part = player.lp.pop();
-	  //   part.x = player[0];
-	  //   part.y = player[1];
-	  //   player.lp.unshift(part);
-		// }
 	}
 }
 
